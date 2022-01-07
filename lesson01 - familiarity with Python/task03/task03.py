6c6b2d91beb0247c44504817974239861d9831ed
Roddg <roddg86@gmail.com>
2022-01-07 20:02:25 +0900

    third task

---
+++ lesson01 - familiarity with Python/task03/task03.py
@@ -0,0 +1,6 @@
+# Задача 2.
+__author__ = "Дмитрий Родионов"
+
+number = input("Введите число >>> ")
+sum_numbers = int(number) + int(number + number) + int(number + number + number)
+print('Cумма чисел n + nn + nnn: ', sum_numbers)

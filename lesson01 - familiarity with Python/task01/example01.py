59c25ef42c70319323c04240d2b4548d877ec44c
Roddg <roddg86@gmail.com>
2022-01-07 15:27:12 +0900

    first task

---
+++ lesson01 - familiarity with Python/task01/example01.py
@@ -0,0 +1,8 @@
+"""
+Примеры из урока
+"""
+
+value = 10 + 5
+print(value)
+value = "Test"  # Динамическая типизация
+print(value)
---
+++ lesson01 - familiarity with Python/task01/task01.py
@@ -0,0 +1,19 @@
+# Задача 1.
+__author__ = "Дмитрий Родионов"
+
+from math import *
+
+a = 1
+b = -2.5
+name = "Вася"
+
+print("%.1f \n%.1f \n%s" % (a, b, name))
+
+name = input("Введите ваше имя: ")
+
+print(name, "найдем значение выражения (x+y)/2(log(x+y)/x*y), введите числа:")
+x = float(input("x = "))
+y = float(input("y = "))
+
+res = (x + y) / 2 * (log(x + y) / x / y) ** 0.5
+print(f"Ответ: {res:.2f}")

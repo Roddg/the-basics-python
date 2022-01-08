fb18342e4cfcfdc7e7e93843696503f0bc854cc9
Roddg <roddg86@gmail.com>
2022-01-08 18:11:56 +0900

    fix task03

--- lesson01 - familiarity with Python/task03/task03.py
+++ lesson01 - familiarity with Python/task03/task03.py
@@ -3,4 +3,6 @@ __author__ = "Дмитрий Родионов"
 
 number = input("Введите число >>> ")
 sum_ = int(number) + int(number + number) + int(number + number + number)
-print(f"Cумма чисел {number} + {number}{number} + {number}{number}{number} = {sum_}")
+print(f"Cумма чисел {number} "
+      f"+ {number}{number} "
+      f"+ {number}{number}{number} = {sum_}")
